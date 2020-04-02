27e5797a6e9024f332e446b91e9fa43e67b16b31
_ipal <malkoleyplay@gmail.com>
2020-04-03 02:16:43 +0300

    update example.c

--- example.c
+++ example.c
@@ -13,48 +13,53 @@ int	print_object(const void *restrict obj_data) {
 	return 0;
 }
 
-int	del_obj1(const void *restrict obj_data) {
+void	free_obj(void *restrict data) {
+	struct s_test	*t = data;
+	free(t->str);
+}
+
+int	match_obj1(const void *restrict obj_data) {
 	const struct s_test *restrict test_data = obj_data;
 	return !(1 == test_data->val && !strcmp("test2", test_data->str));
 }
 
-int	del_obj2(const void *restrict obj_data) {
+int	match_obj2(const void *restrict obj_data) {
 	const struct s_test *restrict test_data = obj_data;
 	return !(2 == test_data->val && !strcmp("test3", test_data->str));
 }
 
-int	del_obj2err(const void *restrict obj_data) {
+int	match_obj2err(const void *restrict obj_data) {
 	const struct s_test *restrict test_data = obj_data;
-	const bool ret = !(2 == test_data->val && !strcmp("test3", test_data->str));
-	if (false == ret)
+	const bool ret = (2 == test_data->val && !strcmp("test3", test_data->str));
+	if (true == ret)
 		return -1;
-	return ret;
+	return !ret;
 }
 
 int	main(void) {
 	struct s_test	s[5] = {
-		{ "test1", 0 },
-		{ "test2", 1 },
-		{ "test3", 2 },
-		{ "test4", 3 },
-		{ "test5", 4 }
+		{ strdup("test1"), 0 },
+		{ strdup("test2"), 1 },
+		{ strdup("test3"), 2 },
+		{ strdup("test4"), 3 },
+		{ strdup("test5"), 4 }
 	};
 	dll_t *restrict	list = dll_init(DLL_GBIT_QUIET);
 
-	dll_pushfront(list, &s[0], sizeof(s[0]), DLL_BIT_DFLT, NULL);
-	dll_pushfront(list, &s[1], sizeof(s[1]), DLL_BIT_DFLT, NULL);
-	dll_pushback(list, &s[2], sizeof(s[2]), DLL_BIT_DFLT, NULL);
-	dll_pushfront(list, &s[3], sizeof(s[3]), DLL_BIT_DFLT, NULL);
-	dll_pushback(list, &s[4], sizeof(s[4]), DLL_BIT_DFLT, NULL);
+	dll_pushfront(list, &s[0], sizeof(s[0]), DLL_BIT_DFLT, free_obj);
+	dll_pushfront(list, &s[1], sizeof(s[1]), DLL_BIT_DFLT, free_obj);
+	dll_pushback(list, &s[2], sizeof(s[2]), DLL_BIT_DFLT, free_obj);
+	dll_pushfront(list, &s[3], sizeof(s[3]), DLL_BIT_DFLT, free_obj);
+	dll_pushback(list, &s[4], sizeof(s[4]), DLL_BIT_DFLT, free_obj);
 	dll_printr(list, print_object);
 
 	printf("\ndeleteing object with val == 1 and str == 'test2':\n");
-	if (dll_delkey(list, del_obj1)) {
+	if (dll_delkey(list, match_obj1)) {
 		printf("\tthe desired object successfully deleted\n");
 	}
 	dll_print(list, print_object);
 	printf("Error test:\n");
-	dll_delkey(list, del_obj2err);
+	dll_delkey(list, match_obj2err);
 
 	printf("\nPrint object founded by index 2 from end:\n");
 	dll_printone(dll_findidr(list, 2), print_object);
