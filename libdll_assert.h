3c2c5e7ffa9685147e656890463d5a694255a003
_ipal <malkoleyplay@gmail.com>
2020-04-05 20:31:23 +0300

    [v5.1] Extended assert message in dll_assert

--- libdll_assert.h
+++ libdll_assert.h
@@ -18,19 +18,26 @@
 
 extern char	*__progname;
 
-static inline void __THROW __attribute__((noreturn)) __dll_assert_fail(void) {
-	unsigned int	errno_line = *__dll_get_errno_line();
-	char *errno_func = *__dll_get_errno_func();
-	char *errno_file = *__dll_get_errno_file();
-	dll_errno_t errno_num = *__dll_get_errno_num();
-	char *dll_err = dll_strerr(errno_num);
+static inline void __THROWNL __attribute__((noreturn))
+__dll_assert_fail(const char *assertion, const char *file,
+		unsigned int line, const char *function) {
+	unsigned int dllerr_line = *__dll_get_errno_line();
+	char *dllerr_func = *__dll_get_errno_func();
+	char *dllerr_file = *__dll_get_errno_file();
+	dll_errno_t dllerr_num = *__dll_get_errno_num();
+	char *dll_err = dll_strerr(dllerr_num);
 	char *str;
 
-	if (0 <= asprintf(&str, "%s%s%s:%u: %s%s%s.\n",
+	if (0 <= asprintf(&str,
+			"%s%s%s:%u: %s%sAssertion `%s` failed.\n"
+			" -- libdll: %s:%u: %s%s%s.\n",
 			__progname, __progname[0] ? ": " : "",
-			errno_file ? errno_file : "", errno_line,
-			errno_func ? errno_func : "",
-			errno_func ? ": " : "", dll_err)) {
+			file, line,
+			function ? function : "", function ? ": " : "",
+			assertion, // end of default assertion error message
+			dllerr_file ? dllerr_file : "", dllerr_line,
+			dllerr_func ? dllerr_func : "",
+			dllerr_func ? ": " : "", dll_err)) {
 		fprintf(stderr, "%s", str);
 		fflush(stderr);
 		free(str);
@@ -43,7 +50,7 @@ static inline void __THROW __attribute__((noreturn)) __dll_assert_fail(void) {
 
 #  define dll_assert(expr) ((expr) \
 	? __ASSERT_VOID_CAST (0) \
-	: __dll_assert_fail())
+	: __dll_assert_fail(#expr, __FILE__, __LINE__, __ASSERT_FUNCTION))
 # endif
 
 #endif /* LIBDLL_ASSERT_H */
