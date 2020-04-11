173c1fbc89172774686285abd8adb1ccaa9f4d3c
Iipal <malkoleyplay@gmail.com>
2020-04-11 10:32:12 +0300

    [v5.4.2]: asserting errors without asprintf

--- libdll_assert.h
+++ libdll_assert.h
@@ -26,25 +26,17 @@ __dll_assert_fail(const char *assertion, const char *file,
 	char *dllerr_file = *__dll_get_errno_file();
 	dll_errno_t dllerr_num = *__dll_get_errno_num();
 	char *dll_err = dll_strerr(dllerr_num);
-	char *str;
 
-	if (0 <= asprintf(&str,
-			"%s%s%s:%u: %s%sAssertion `%s` failed.\n"
-			" -- libdll: %s:%u: %s%s%s.\n",
-			__progname, __progname[0] ? ": " : "",
-			file, line,
-			function ? function : "", function ? ": " : "",
-			assertion, // end of default assertion error message
-			dllerr_file ? dllerr_file : "", dllerr_line,
-			dllerr_func ? dllerr_func : "",
-			dllerr_func ? ": " : "", dll_err)) {
-		fprintf(stderr, "%s", str);
-		fflush(stderr);
-		free(str);
-	} else {
-		static const char	errstr[] = "Unexpected error\n";
-		write(STDERR_FILENO, errstr, sizeof(errstr) - 1);
-	}
+	fprintf(stderr, "%s%s%s:%u: %s%sAssertion `%s` failed.\n"
+		" -- libdll: %s:%u: %s%s%s.\n",
+		__progname, __progname[0] ? ": " : "",
+		file, line,
+		function ? function : "", function ? ": " : "",
+		assertion, // end of default assertion error message
+		dllerr_file ? dllerr_file : "", dllerr_line,
+		dllerr_func ? dllerr_func : "",
+		dllerr_func ? ": " : "", dll_err);
+	fflush(stderr);
 	abort();
 }
 
