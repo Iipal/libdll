6f13059bcafe68fb3c21c42bf7065f0e868cbbba
Iipal <malkoleyplay@gmail.com>
2020-04-16 22:49:06 +0300

    [v7.5] added a few assert variations

--- libdll_assert.h
+++ libdll_assert.h
@@ -31,32 +31,73 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+
 # include "libdll_errno.h"
+# include "libdll_bits.h"
+
+typedef enum {
+	__dll_assert_tnormal             = 1,
+	__dll_assert_tsoft               = 2,
+	__dll_assert_tsoft_normal        = 3,
+	__dll_assert_tperror             = 4,
+	__dll_assert_tperror_normal      = 5,
+	__dll_assert_tperror_soft        = 6,
+	__dll_assert_tperror_soft_normal = 7,
+} __attribute__((packed)) __dll_internal_assert_type;
 
 extern char	*__progname;
 
-static inline void __THROWNL __attribute__((noreturn))
-__dll_assert_fail(const char *assertion, const char *file,
-		unsigned int line, const char *function) {
-	__dll_internal_errdata_t *restrict	errdata = __dll_geterrdata();
-	char	*err = dll_strerr(errdata->__errcode);
-
-	fprintf(stderr, "%s%s%s:%u: %s%sAssertion `%s` failed.\n"
-		" -- libdll: %s:%u: %s%s%s.\n",
-		__progname, __progname[0] ? ": " : "",
-		file, line,
-		function ? function : "", function ? ": " : "",
-		assertion, // end of default assertion error message
-		errdata->__errfile ? errdata->__errfile : "", errdata->__errln,
-		errdata->__errfn ? errdata->__errfn : "",
-		errdata->__errfn ? ": " : "", err);
+static inline void	__dll_assert_fail(const char *assertion,
+		const char *file,
+		unsigned int line,
+		const char *function,
+		__dll_internal_assert_type atype) {
+	if (__dll_is_bit(atype, __dll_assert_tnormal)) {
+		fprintf(stderr, "%s%s%s:%u: %s%sAssertion `%s` failed.\n",
+			__progname, __progname[0] ? ": " : "", file, line,
+			function ? function : "", function ? ": " : "", assertion);
+	}
+	if (__dll_is_bit(atype, __dll_assert_tperror)) {
+		__dll_internal_errdata_t *restrict errdata = __dll_internal_geterrdata();
+		fprintf(stderr, " -- libdll: %s%s%u: %s%s%s.\n",
+			errdata->__errfile, errdata->__errfile[0] ? ": " : "",
+			errdata->__errln,
+			errdata->__errfn ? errdata->__errfn : "",
+			errdata->__errfn ? ": " : "", dll_strerr(errdata->__errcode));
+	}
 	fflush(stderr);
-	abort();
+	if (!__dll_is_bit(atype, __dll_assert_tsoft)) {
+		abort();
+	}
 }
 
 #  define dll_assert(expr) ((expr) \
 	? __ASSERT_VOID_CAST (0) \
-	: __dll_assert_fail(#expr, __FILE__, __LINE__, __ASSERT_FUNCTION))
+	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
+		__ASSERT_FUNCTION, __dll_assert_tnormal))
+
+#  define dll_assert_soft(expr) ((expr) \
+	? __ASSERT_VOID_CAST (0) \
+	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
+		__ASSERT_FUNCTION, __dll_assert_tsoft_normal))
+
+#  define dll_assert_perror(expr) ((expr) \
+	? __ASSERT_VOID_CAST (0) \
+	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
+		__ASSERT_FUNCTION, __dll_assert_tperror_normal))
+
+#  define dll_assert_soft_perror(expr) ((expr) \
+	? __ASSERT_VOID_CAST (0) \
+	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
+		__ASSERT_FUNCTION, __dll_assert_tperror_soft_normal))
+
 # endif
 
 #endif /* LIBDLL_ASSERT_H */
+# ifndef __clang__
+/**
+ * GCC doesn't allow to fall through switch-cases
+ * This attribute required to ignore compiler errors about this.
+ */
+			__attribute__((fallthrough));
+# endif /* __clang__ */
