e22a2b5a98b091539504f80269b03424d2b6e76e
Iipal <malkoleyplay@gmail.com>
2020-04-20 14:03:37 +0300

    [v7.7] dll_assert and dll_assert_soft only

--- libdll_assert.h
+++ libdll_assert.h
@@ -35,61 +35,36 @@
 # include "libdll_errno.h"
 # include "libdll_bits.h"
 
-typedef enum {
-	__dll_assert_tnormal             = 1,
-	__dll_assert_tsoft               = 2,
-	__dll_assert_tsoft_normal        = 3,
-	__dll_assert_tperror             = 4,
-	__dll_assert_tperror_normal      = 5,
-	__dll_assert_tperror_soft        = 6,
-	__dll_assert_tperror_soft_normal = 7,
-} __attribute__((packed)) __dll_internal_assert_type;
-
 extern char	*__progname;
 
 static inline void	__dll_assert_fail(const char *assertion,
 		const char *file,
 		unsigned int line,
 		const char *function,
-		__dll_internal_assert_type atype) {
-	if (__dll_is_bit(atype, __dll_assert_tnormal)) {
-		fprintf(stderr, "%s%s%s:%u: %s%sAssertion `%s` failed.\n",
-			__progname, __progname[0] ? ": " : "", file, line,
-			function ? function : "", function ? ": " : "", assertion);
-	}
-	if (__dll_is_bit(atype, __dll_assert_tperror)) {
-		__dll_internal_errdata_t *restrict errdata = __dll_internal_geterrdata();
-		fprintf(stderr, " -- libdll: %s%s%u: %s%s%s.\n",
-			errdata->__errfile, errdata->__errfile[0] ? ": " : "",
-			errdata->__errln,
-			errdata->__errfn ? errdata->__errfn : "",
-			errdata->__errfn ? ": " : "", dll_strerr(errdata->__errcode));
-	}
+		bool is_soft) {
+	__dll_internal_errdata_t *restrict errdata = __dll_internal_geterrdata();
+	fprintf(stderr, "%s%s%s:%u: %s%sAssertion `%s` failed.\n",
+		__progname, __progname[0] ? ": " : "", file, line,
+		function ? function : "", function ? ": " : "", assertion);
+	fprintf(stderr, " -- libdll: %s%s%u: %s%s%s.\n",
+		errdata->__errfile, errdata->__errfile[0] ? ": " : "",
+		errdata->__errln,
+		errdata->__errfn ? errdata->__errfn : "",
+		errdata->__errfn ? ": " : "", dll_strerr(errdata->__errcode));
 	fflush(stderr);
-	if (!__dll_is_bit(atype, __dll_assert_tsoft)) {
+	if (!is_soft)
 		abort();
-	}
 }
 
 #  define dll_assert(expr) ((expr) \
 	? __ASSERT_VOID_CAST (0) \
 	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
-		__ASSERT_FUNCTION, __dll_assert_tnormal))
+		__ASSERT_FUNCTION, false))
 
 #  define dll_assert_soft(expr) ((expr) \
 	? __ASSERT_VOID_CAST (0) \
 	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
-		__ASSERT_FUNCTION, __dll_assert_tsoft_normal))
-
-#  define dll_assert_perror(expr) ((expr) \
-	? __ASSERT_VOID_CAST (0) \
-	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
-		__ASSERT_FUNCTION, __dll_assert_tperror_normal))
-
-#  define dll_assert_soft_perror(expr) ((expr) \
-	? __ASSERT_VOID_CAST (0) \
-	: __dll_assert_fail(#expr, __FILE__, __LINE__, \
-		__ASSERT_FUNCTION, __dll_assert_tperror_soft_normal))
+		__ASSERT_FUNCTION, true))
 
 # endif
 
